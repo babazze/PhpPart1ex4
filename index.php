fef6c6232e137c9ad68950251e57522eed7fcbe4
babazze <babazze@live.fr>
2019-12-02 16:06:22 +0100

    go

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$name = 'Azz';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>ex3</title>
+  </head>
+  <body>
+      <p>Bonjour <?= $name; ?> comment vas-tu ?</p>
+  </body>
+</html>
